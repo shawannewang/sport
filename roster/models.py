eb39d35c822ffa39a63985f3c2d5553e23fd731d
shawannw <shawannw@ubuntu.ubuntu-domain>
2014-03-02 14:57:45 -0500

    adding more

--- roster/models.py
+++ roster/models.py
@@ -8,14 +8,18 @@ class Member(models.Model):
 	year = models.CharField(unique=False, max_length=10)
 	position = models.CharField(unique=False, max_length=20)
 	height = models.TextField(unique=False, max_length=3)
+	kill = models.TextField(unique=False)
+	attack = models.TextField(unique=False)
+	block = models.TextField(unique=False)
+	dig = models.TextField(unique=False)
 	imageurl = models.TextField(unique=False, null=True)
 
 	class Meta(object):
 		verbose_name_plural = 'Members'
-		ordering = ('name', 'number', 'year', 'height', 'position')
+		ordering = ('name', 'number', 'year', 'height', 'position', 'kill', 'attack', 'block', 'dig')
 
 	def __unicode__(self):
-		return U'%s %s %s %s %s' %(self.name, self.number, self.year, self.height, self.position)
+		return U'%s %s %s %s %s %s %s %s %s' %(self.name, self.number, self.year, self.height, self.position, self.kill, self.attack, self.block, self.dig)
 
 	def save(self, *args, **kwargs):
 		self.name = self.name.upper()
